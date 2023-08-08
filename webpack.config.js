a2bd7650ab8118d93c5a45bf4fb275a5344e0f77
Kayode Ogunlade <109389152+Drk-codey@users.noreply.github.com>
2023-08-08 01:21:48 +0100

    Add file and html loader

--- webpack.config.js
+++ webpack.config.js
@@ -10,4 +10,28 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'dist/images/'
+            }
+          },
+        ],
+      },
+      {
+        test:/\.html$/,
+        use: [
+          // 'html-loader'
+        ]
+      },
+      
+    ],
+  },
+  
 };
