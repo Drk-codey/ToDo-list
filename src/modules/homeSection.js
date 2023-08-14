0d17bb89d4f56ea0b9053f7dbf6210525ae209f9
Kayode Ogunlade <109389152+Drk-codey@users.noreply.github.com>
2023-08-14 23:05:16 +0100

    Add home section

---
+++ src/modules/homeSection.js
@@ -0,0 +1,39 @@
+
+
+function inboxAllTask() {
+  const inboxText = document.getElementById("title");
+  inboxText.textContent = "Inbox";
+}
+
+function initializepage() {
+  const projectPreview = document.getElementById("project-preview");
+  projectPreview.innerHTML = `
+    <div class="project-title">
+      <h1 id="title"></h1>
+    </div>
+    <div id="todo-container">
+      <div id="todo-list"></div>
+      <div class="add-task-popup" id="add-task-popup">
+        <input type="text" 
+          class="input-taskName" 
+          id="input-taskName" 
+          placeholder="Enter Project Title"
+        >
+        <textarea id="taskDesc" cols="63" rows="4"></textarea>
+        <input type="date" class="input-taskDate" id="input-taskDate">
+        <div class="add-task-popup-buttons">
+          <button class="button-add-task" id="button-add-task">Add</button>
+          <button class="button-cancel-task" id="button-cancel-task">Cancel</button>
+        </div>
+      </div>
+    </div>
+
+    <div class="tiles" id="add-new-task">
+      <img src="/images/plus-icon.png" alt="" class="icon">
+      <p class="data-name">Add Task</p>
+    </div> 
+  `
+  inboxAllTask();
+}
+
+export { initializepage };
